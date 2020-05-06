ba70cc555479052d7e3f919caedf725d8a446665
Ben Stoica <bennystoica@gmail.com>
2020-05-05 23:01:43 -0400

    ConsoleHangman

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Spaceman
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+            Game g = new Game();
+            g.Greet();
+
+            do
+            {
+                g.Display();
+                g.Ask();
+                if (g.DidLose())
+                {
+                    g.Display();
+                    Console.WriteLine("You lost, they took you away!");
+                    break;
+                }
+                else if (g.DidWin())
+                {
+                    g.Display();
+                    Console.WriteLine("You get to stay on Earth...this time!");
+                    break;
+                }
+            } while(true);
+    }
+  }
+}
